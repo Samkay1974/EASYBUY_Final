b7597e57827be4361802adb8404e3d066acd18e8
Samkay1974 <samuel.ninson@ashesi.edu.gh>
2025-11-30 13:07:53 +0000

    minor movements
    moving some files to different directory for easy identification

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -20,13 +20,13 @@ $current_page = basename($_SERVER['PHP_SELF']);
         <a class="nav-link <?php echo $current_page == 'categories.php' ? 'active' : ''; ?>" href="categories.php">
             <i class="fas fa-folder me-2"></i>Manage Categories
         </a>
-        <a class="nav-link position-relative <?php echo $current_page == 'reports.php' ? 'active' : ''; ?>" href="reports.php">
+        <a class="nav-link position-relative <?php echo $current_page == '../view/reports.php' ? 'active' : ''; ?>" href="reports.php">
             <i class="fas fa-flag me-2"></i>Reports
             <?php if ($pending_reports > 0): ?>
                 <span class="badge-notification"><?php echo $pending_reports; ?></span>
             <?php endif; ?>
         </a>
-        <a class="nav-link <?php echo $current_page == 'analytics.php' ? 'active' : ''; ?>" href="analytics.php">
+        <a class="nav-link <?php echo $current_page == '../view/analytics.php' ? 'active' : ''; ?>" href="analytics.php">
             <i class="fas fa-chart-line me-2"></i>Analytics
         </a>
         <a class="nav-link" href="../view/homepage.php">
